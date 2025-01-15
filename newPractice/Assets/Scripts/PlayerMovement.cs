3d28535c8ccb93ef74b2e18d6a0ec35a0cf23b6a
emreyilmaz2006 <1764782@fife.ac.uk>
2025-01-15 10:19:24 +0000

    Added Joystick
    added joystick which can move player left and right

--- newPractice/Assets/Scripts/PlayerMovement.cs
+++ newPractice/Assets/Scripts/PlayerMovement.cs
@@ -20,14 +20,32 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private LayerMask GroundLayer = 0;
 
+    [SerializeField]
+    private Joystick myJoystick = null;
+
     private Rigidbody2D physicsBody = null;
 
+    //unity functions
     private void Awake()
     {
         //getting the rigidbody2d component attached to the same object as this script (player)
         physicsBody = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        //make a variable to hold velocity
+        //get current velocity from physics component
+        Vector2 newVelocity = physicsBody.velocity;
+
+        //set velocity to move in the direction our joystick is pressed
+        newVelocity.x = speed * myJoystick.Horizontal;
+
+        //update physics component's velocity to be our newly changed value
+        physicsBody.velocity = newVelocity;
+    }
+
+    //user defined functions
     public void MoveLeft()
     {
         //make a variable to hold velocity
